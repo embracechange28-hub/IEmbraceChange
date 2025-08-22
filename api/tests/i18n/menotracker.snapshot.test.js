610323705b34f923ca841a7e5ca4647f110f276b
Alison Copoc <embracechange28@gmail.com>
2025-08-22 12:16:52 -0400

    Add files via upload

--- api/tests/i18n/menotracker.snapshot.test.js
+++ api/tests/i18n/menotracker.snapshot.test.js
@@ -1,1 +1,34 @@
+const menotracker = require('../../locales/en/menotracker.json');
+const { menotrackerKeys } = require('../../types/i18n/menotracker.keys.cjs');
 
+function flatten(obj, prefix = '') {
+  const out = {};
+  if (obj && typeof obj === 'object' && !Array.isArray(obj)) {
+    for (const [k, v] of Object.entries(obj)) {
+      const p = prefix ? `${prefix}.${k}` : k;
+      Object.assign(out, flatten(v, p));
+    }
+  } else {
+    out[prefix] = true;
+  }
+  return out;
+}
+
+describe('i18n: menotracker', () => {
+  test('canonical copy snapshot', () => {
+    expect(menotracker).toMatchSnapshot();
+  });
+
+  test('contains all defined keys', () => {
+    const flat = flatten(menotracker);
+    menotrackerKeys.forEach(k => {
+      expect(flat).toHaveProperty(k);
+    });
+  });
+
+  test('no extra unexpected keys (optional)', () => {
+    const flat = Object.keys(flatten(menotracker));
+    const extras = flat.filter(k => !menotrackerKeys.includes(k));
+    expect(extras).toEqual([]);
+  });
+});
